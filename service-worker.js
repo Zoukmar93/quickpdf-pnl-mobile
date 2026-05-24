2660131c74a66023586c948ef2a1935ae716a7f0
Zoukmar93 <m.rabahi@alyzia.com>
2026-05-24 22:18:54 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='quickpdf-pnl-mobile-v1';
-const FILES=['./','./index.html','./manifest.json','./icon.svg','./alyzia_logo.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).catch(()=>null)));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
